393eec1680e6c612f31cf5ddb488d3aad340153e
Remek2GO <101603785+Remek2GO@users.noreply.github.com>
2022-11-28 18:22:56 +0100

    Uzupełnienie o klasę Client i Product

--- servers.py
+++ servers.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
  
 from typing import Optional, List, Dict
-from abc import ABC, abstractmethod
+from abc import TypeVar, ABC, abstractmethod
 import re
  
 # FIXME: Każada z poniższych klas serwerów powinna posiadać:
 #   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
 #   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
+
 
 class ServerError(Exception):
     pass 
@@ -17,6 +18,12 @@ class TooManyProductsFoundError(ServerError):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
+class Product:
+    def __init__(self, name: str, price: float) -> None: #metoda inicjalizacyjna
+        if name.isdigit() or name.isalpha() or name == None: # sprawdzam czy nazwa jest poprawna
+            raise ValueError
+        self.name: str = name
+        self.price: float = price
 
 class Server(ABC): #klasa abstrakcyjna
     n_max_returned_entries = 3
@@ -40,7 +47,20 @@ class Server(ABC): #klasa abstrakcyjna
     def _get_products(self,n_letters: int = 1)-> List[Product]:
         raise NotImplementedError
 
+ServerType = TypeVar('ServerType', bound=Server) #Użycie TypeVar aby otrzymać ten sam typ serwera
 
+
+class Client:
+    def __init__(self, server: ServerType) -> None: # metoda inicjalizacyjna
+        self.server: ServerType = server
+
+    def get_total_price(self, n_letters: Optional[int]) -> float:
+        try:
+            entries = self.server.get_entries() if n_letters is None \
+                else self.server.get_entries(n_letters)
+            return sum([entry.price for entry in entries])
+        except TooManyProductsFoundError:
+            return 0
 
 class ListServer(Server):
 
